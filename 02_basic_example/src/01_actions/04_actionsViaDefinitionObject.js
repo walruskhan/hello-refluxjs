387128ccbe9f2864760669621781e4e226ade0dd
Greg C <gregjc@protonmail.com>
2023-04-30 20:06:14 +1200

    Added trigger and triggerAsync example to 01_actions/04_actionsViaDefinitionObject.js

--- 02_basic_example/src/01_actions/04_actionsViaDefinitionObject.js
+++ 02_basic_example/src/01_actions/04_actionsViaDefinitionObject.js
@@ -17,3 +17,12 @@ Actions.foo.listen(function(){
 
 Actions.foo(1, 2, 3, 4, 5);
 
+// can also invoke an action via trigger() and triggerAsync()
+// the trigger methods are called by Actions.foo() based on whether sync=true (trigger) or false (triggerAsync)
+console.log("1");
+Actions.foo.trigger("trigger")
+console.log("2");
+Actions.foo.triggerAsync("triggerAsync"); // this is called after "3" because it is queued via _.tick() internally
+                                          // i.e. different from promise-based async
+console.log("3");
+
